19a645cd7ea11b6fda9865cdc4ad29337f60463a
Amir Muhawesh <amuhawesh@gmail.com>
2015-06-18 15:25:39 -0500

    Initial commit

---
+++ app/src/main/java/com/example/puter/sunshine/app/sync/SunshineAuthenticatorService.java
@@ -0,0 +1,21 @@
+package com.example.puter.sunshine.app.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SunshineAuthenticatorService extends Service {
+
+    private SunshineAuthenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+
+        mAuthenticator = new SunshineAuthenticator(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+}
